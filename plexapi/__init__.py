233ceafa4a70ef312b7490a9fd5d498b9d60e50e
Elan Ruusamäe <glen@pld-linux.org>
2021-03-21 20:32:14 +0200

    Remove now unused X_* headers from __init__.py

--- plexapi/__init__.py
+++ plexapi/__init__.py
@@ -2,8 +2,6 @@
 import logging
 import os
 from logging.handlers import RotatingFileHandler
-from platform import uname
-from uuid import getnode
 
 from plexapi.config import PlexConfig, reset_base_headers
 from plexapi.utils import SecretsFilter
@@ -20,15 +18,6 @@ TIMEOUT = CONFIG.get('plexapi.timeout', 30, int)
 X_PLEX_CONTAINER_SIZE = CONFIG.get('plexapi.container_size', 100, int)
 X_PLEX_ENABLE_FAST_CONNECT = CONFIG.get('plexapi.enable_fast_connect', False, bool)
 
-# Plex Header Configuation
-X_PLEX_PROVIDES = CONFIG.get('header.provides', 'controller')
-X_PLEX_PLATFORM = CONFIG.get('header.platform', CONFIG.get('header.platorm', uname()[0]))
-X_PLEX_PLATFORM_VERSION = CONFIG.get('header.platform_version', uname()[2])
-X_PLEX_PRODUCT = CONFIG.get('header.product', PROJECT)
-X_PLEX_VERSION = CONFIG.get('header.version', VERSION)
-X_PLEX_DEVICE = CONFIG.get('header.device', X_PLEX_PLATFORM)
-X_PLEX_DEVICE_NAME = CONFIG.get('header.device_name', uname()[1])
-X_PLEX_IDENTIFIER = CONFIG.get('header.identifier', str(hex(getnode())))
 BASE_HEADERS = reset_base_headers()
 
 # Logging Configuration
